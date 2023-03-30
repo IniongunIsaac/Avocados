d9958793a74f341e1efa54282e1c78d649452f7f
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-30 20:18:18 +0100

    create main tabview

---
+++ Avocados/Views/RipeningStagesView.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningStagesView.swift
+//  Avocados
+//
+//  Created by Isaac Iniongun on 30/03/2023.
+//
+
+import SwiftUI
+
+struct RipeningStagesView: View {
+    var body: some View {
+        Text("Ripening Stages")
+    }
+}
+
+struct RipeningStagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStagesView()
+    }
+}
---
+++ Avocados/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Avocados
+//
+//  Created by Isaac Iniongun on 30/03/2023.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
