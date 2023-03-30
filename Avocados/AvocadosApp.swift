a391351669d5cfd469290253d25a6660efac58ad
Isaac Iniongun <iniongunisaac@gmail.com>
2023-03-30 18:22:26 +0100

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Isaac Iniongun on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
